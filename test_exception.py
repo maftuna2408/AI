6e3cf0081a7c92f038d557b4157200930fe7c448
maftuna2408 <tunchik2408@gmail.com>
2025-05-15 18:27:50 +0900

    okay

--- test_exception.py
+++ test_exception.py
@@ -9,7 +9,7 @@ def divide(x, y):
 
 if __name__ == "__main__":
     try:
-        result = divide(10, 0)
+        result = divide(10, 0) # 0 ga bo'lib bo'lmaydi shunga error chiqardi, 0 o'zgarsa error yo'q
         print(f"{Fore.GREEN}Natija: {result}")
     except Exception as e:
         custom_exc = CustomException(e, sys)
